7ead5ce34f3cbc225685692cc7aa7c72e29dc0bc
maskudo <manujkarki101@gmail.com>
2024-05-11 21:30:12 +0545

    feat: abstraction for printing to vga and print hello world

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@
 
 use core::panic::PanicInfo;
 
+use vga_buffer::print_something;
+mod vga_buffer;
+
 // function called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -14,14 +17,7 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            // first byte character and second byte color
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    print_something();
 
     loop {}
 }
