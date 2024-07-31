2e284a15fb87f0d5d98ac03f47676e23ab25b962
maskudo <manujkarki101@gmail.com>
2024-07-31 20:48:36 +0545

    feat: page fault handler

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,9 @@ pub extern "C" fn _start() -> ! {
     println!("hello wolr{}", "d");
 
     toyos::init();
+    use x86_64::registers::control::Cr3;
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table);
 
     #[cfg(test)]
     test_main();
